27f4e6069f206d7b49684f6d04cbad74ea26e971
Chuanlong Zhou <czhou@obelix4.lsce.ipsl.fr>
2023-02-15 11:21:25 +0100

    DEA

--- utils.py
+++ utils.py
@@ -55,8 +55,9 @@ def display(gdf, image_path):
     for i, r in gdf.iterrows():
         # print(i)
         my_bar.progress((i + 1)/len(gdf), text=progress_text)
-        if i%3!=0 or r["file"] is None:
-            continue
+
+        # if i%3!=0 or r["file"] is None:
+        #     continue
         # geodataframe of row
         gdf_ = geopandas.GeoDataFrame(r.to_frame().T, crs=gdf.crs)
         
